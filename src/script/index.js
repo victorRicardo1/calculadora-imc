26111a149778bbc6fdf4d0569065e47da2405e50
Ricardo Victor <ricardovictor737@gmail.com>
2024-01-04 17:54:10 -0300

    project tree builded

---
+++ src/script/index.js
@@ -0,0 +1,9 @@
+const alturaInp = document.getElementById('altura');
+const pesoInp = document.getElementById('peso');
+
+const calcBtn = document.getElementById('calcBtn');
+const imcResultEl = document.querySelector('.imcResult');
+const concResult = document.querySelector('.conclusao');
+
+alturaInp.value = 5
+console.log(alturaInp.value)
