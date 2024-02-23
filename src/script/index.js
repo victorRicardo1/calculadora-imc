ff80946cd744b69a213bbe5d1ebad27a2874d7fc
Ricardo Victor <ricardovictor737@gmail.com>
2024-02-23 19:17:47 -0300

    Finished

--- src/script/index.js
+++ src/script/index.js
@@ -12,5 +12,22 @@ calcBtn.addEventListener('click', ()=>{
     const pesoValue = parseFloat(pesoInp.value);
     imc = pesoValue / (alturaValue) ** 2;
     imcResultEl.textContent = imc.toFixed(2);
+    if(alturaInp == 'null' && pesoInp == 'null'){
+        imcResultEl.textContent = '0'
+    }
+
+    if(imc < 18.5){
+        concResult.textContent = 'Você está abaixo do peso!';
+    }else if(imc >= 18.6 && imc <= 24.9){
+        concResult.textContent = 'Você está no peso ideal, parabéns!';
+    }else if(imc >= 25 && imc <= 29.9){
+        concResult.textContent = 'Você está levemente acima do peso';
+    }else if(imc >= 30 && imc <= 34.9){
+        concResult.textContent = 'Você está com obesidade grau 1';
+    }else if(imc >= 35 && imc <= 39.9){
+        concResult.textContent = 'Você está com obesidade severa';
+    }else if(imc >= 40){
+        concResult.textContent = 'Você está com obesidade mórbida';
+    }
 })
 
